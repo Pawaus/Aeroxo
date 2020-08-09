a5f1ad81ecc58e59174f4cf4fd4906dec55d6c0a
Pawaus <pawa.usov2013@gmail.com>
2020-08-09 17:45:35 +0300

    New class parce JSON

--- app/src/main/java/com/pawa/aeroxo/JSONsheets.java
+++ app/src/main/java/com/pawa/aeroxo/JSONsheets.java
@@ -1,8 +1,39 @@
 package com.pawa.aeroxo;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JSONsheets {
     public String[][] data;
-    JSONsheets(String parcebleString){
+    private JSONObject localJson;
+
+
+    JSONsheets(JSONObject jsonObject){
         //TODO:json парсить тут
+        localJson = jsonObject;
+    }
+    public String getStatus(){
+        try {
+            return localJson.getString("status");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+
+    public String getLastStatus(){
+
+        try {
+            JSONObject data = null;
+            data = localJson.getJSONObject("data");
+            JSONObject lastPoint = data.getJSONObject("lastPoint");
+            String lastPos = lastPoint.getString("operation");
+            lastPos = lastPos + ", " +lastPoint.getString("eventDateTime");
+            return lastPos;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "error";
+        }
+
     }
 }
