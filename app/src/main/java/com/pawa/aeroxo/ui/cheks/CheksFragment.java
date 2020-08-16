9c5680120305dcb5ba228f53c58956918cba2038
Pawaus <pawa.usov2013@gmail.com>
2020-08-16 17:55:25 +0300

    checks update

--- app/src/main/java/com/pawa/aeroxo/ui/cheks/CheksFragment.java
+++ app/src/main/java/com/pawa/aeroxo/ui/cheks/CheksFragment.java
@@ -1,8 +1,11 @@
 package com.pawa.aeroxo.ui.cheks;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -10,12 +13,15 @@ import androidx.annotation.NonNull;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -24,6 +30,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 
@@ -47,12 +55,15 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.pawa.aeroxo.MainActivity;
 import com.pawa.aeroxo.R;
+import com.pawa.aeroxo.bd.Check;
+import com.pawa.aeroxo.bd.Track;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.util.List;
 import java.util.Objects;
 
 public class CheksFragment extends Fragment implements View.OnClickListener{
@@ -61,53 +72,41 @@ public class CheksFragment extends Fragment implements View.OnClickListener{
     private FirebaseAuth mAuth;
     private static final int CAMERA_PIC_REQUEST=1112;
     private static final int RC_HANDLE_CAMERA_PERM = 2;
-    private Uri outputFileUri;
-    private StorageReference mStorageRef;
+    private SharedPreferences sharedPreferences;
+    LinearLayout linearLayoutChecks;
 
 
-
-    ImageView image;
-    EditText textEmail,textPasswd;
-    private Button btnAuth,btnReg,btnGetData,btnSendData,btnCam;
-    String fromBase;
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode==RC_HANDLE_CAMERA_PERM) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            return;
-        }else{
-            requestReadStoragePermission();
-            requestWriteStoragePermission();
-            requestCameraPermission();
-        }
-    }
-
-    @RequiresPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    //@RequiresPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        cheksViewModel =
-                ViewModelProviders.of(this).get(CheksViewModel.class);
         View root = inflater.inflate(R.layout.fragment_cheks, container, false);
-        //image =(ImageView) root.findViewById(R.id.imageView);
-        //textName = (TextView)root.findViewById(R.id.textName);
-        mAuth = FirebaseAuth.getInstance();
-        //btnCam = (Button)root.findViewById(R.id.buttonCam);
-        //btnCam.setOnClickListener(this);
-        requestReadStoragePermission();
-        requestWriteStoragePermission();
-        requestCameraPermission();
-        mStorageRef = FirebaseStorage.getInstance().getReference();
-        outputFileUri = null;
-        //root.findViewById(R.id.buttonSendPhoto).setOnClickListener(this);
-        //root.findViewById(R.id.buttonSendData).setOnClickListener(this);
+        linearLayoutChecks = (LinearLayout)root.findViewById(R.id.linearChecks);
         return root;
     }
 
-    @RequiresPermission(Manifest.permission.CAMERA)
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        cheksViewModel = new ViewModelProvider(getActivity()).get(CheksViewModel.class);
+        sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+
+        cheksViewModel.startUpdate(sharedPreferences.getString("fullName",""));
+        cheksViewModel.getChecks().observe(getActivity(), new Observer<List<Check>>() {
+            @Override
+            public void onChanged(List<Check> checks) {
+                linearLayoutChecks.removeAllViews();
+                Log.d("checks","checks "+ String.valueOf(checks.size()));
+                for(Check check:checks){
+                    AddViewOnScroll(check);
+                }
+            }
+        });
+    }
 
     @Override
     public void onClick(View view) {
+        //Работа с камерой и отсылкой данных
         /*if(view.getId()==R.id.buttonCam){
             //Intent cameraIntent=new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
             //if(requestCameraPermission()&&requestWriteStoragePermission()&&requestReadStoragePermission())
@@ -117,24 +116,19 @@ public class CheksFragment extends Fragment implements View.OnClickListener{
         if (view.getId()==R.id.buttonSendPhoto){
             getRealtimeData();
         }
-        if(view.getId()==R.id.buttonSendData){
-            /*FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference myRef = database.getReference("myArray");
-            incVal(fromBase,myRef);*/
-
         //}*/
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //при возвращении изображения с камеры
+        /*
         if (requestCode==CAMERA_PIC_REQUEST) {
-            assert data != null;
-            //Bitmap thumbnail = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
-            //image.setImageBitmap(thumbnail);
-            image.setImageURI(outputFileUri);
-        }
+        }*/
     }
+    //Сохранение фотографии после съемки
+    /*
     private void saveFullImage() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         File file = new File(Environment.getExternalStorageDirectory(), "test.jpg");
@@ -142,7 +136,9 @@ public class CheksFragment extends Fragment implements View.OnClickListener{
         intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
 
         startActivityForResult(intent, CAMERA_PIC_REQUEST);
-    }
+    }*/
+    //Запрос разрешения на Камеру
+    /*
     private boolean requestCameraPermission() {
 
         final String[] permissions = new String[]{Manifest.permission.CAMERA};
@@ -153,7 +149,9 @@ public class CheksFragment extends Fragment implements View.OnClickListener{
             return true;
         }
         return false;
-    }
+    }*/
+    //Запрос разрешения на память
+    /*
     private boolean requestWriteStoragePermission() {
 
         final String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
@@ -165,6 +163,7 @@ public class CheksFragment extends Fragment implements View.OnClickListener{
         }
         return false;
     }
+
     private boolean requestReadStoragePermission() {
 
         final String[] permissions = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
@@ -175,8 +174,9 @@ public class CheksFragment extends Fragment implements View.OnClickListener{
             return true;
         }
         return false;
-    }
-    private void SendPhotoByUri(Uri uri){
+    }*/
+    //Отсылка фотографии в БД
+    /*private void SendPhotoByUri(Uri uri){
         StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
         riversRef.putFile(outputFileUri)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -195,61 +195,31 @@ public class CheksFragment extends Fragment implements View.OnClickListener{
                         Toast.makeText(getActivity(),"upload fail",Toast.LENGTH_LONG).show();
                     }
                 });
-    }
+    }*/
 
-    private void getRealtimeData(){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference("myArray");
-        //final String[] fromBase = new String[1];
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-          //      fromBase[0] = dataSnapshot.getValue(String.class);
-                fromBase = value;
-                //incVal(value,myRef);
-                //Toast.makeText(getActivity(),"recieved",Toast.LENGTH_LONG).show();
-            }
+private void AddViewOnScroll(Check check){
+    LinearLayout linearLayout = new LinearLayout(getActivity());
+    linearLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+    linearLayout.setOrientation(LinearLayout.VERTICAL);
+    TextView textNameCheck = new TextView(getContext());
+    TextView textDate = new TextView(getContext());
+    TextView textSum = new TextView(getContext());
+    TextView textIsViewed = new TextView(getContext());
+    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+    linearLayout.setBackground(getActivity().getDrawable(R.drawable.linearprofile2));
+    layoutParams.setMargins(32,32,16,32);
+    textNameCheck.setText(check.nameCheck);
+    textNameCheck.setTextSize(TypedValue.COMPLEX_UNIT_SP,20);
+    Typeface boldTypeface = Typeface.defaultFromStyle(Typeface.BOLD);
+    textDate.setText(check.Sum + " от "+check.Date);
+    textDate.setTypeface(boldTypeface);
+    textDate.setTextSize(TypedValue.COMPLEX_UNIT_SP,16);
+    textIsViewed.setText((check.isViewed)?"Учтен":"Не учтен");
+    textIsViewed.setTextSize(TypedValue.COMPLEX_UNIT_SP,16);
+    linearLayout.addView(textNameCheck,layoutParams);
+    linearLayout.addView(textDate,layoutParams);
+    linearLayout.addView(textIsViewed,layoutParams);
+    linearLayoutChecks.addView(linearLayout,layoutParams);
+}
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                //Toast.makeText(getActivity(),"cancelled",Toast.LENGTH_LONG).show();
-                return;
-            }
-        });
-        //incVal(fromBase,myRef);
-        /**/
-
-    }
-    public void incVal(String getfromBase,DatabaseReference myRef){
-        try {
-            JSONObject reader = new JSONObject(getfromBase);
-            JSONArray array = reader.getJSONArray("result");
-            JSONArray result = new JSONArray();
-            for(int i = 0;i<array.length();i++){
-                String tmp = array.getJSONArray(i).getString(0);
-                if(tmp.equals("mtr1")){
-                    int t = array.getJSONArray(i).getInt(2);
-                    Toast.makeText(getActivity(),Integer.toString(t),Toast.LENGTH_LONG).show();
-                    JSONArray inArray = array.getJSONArray(i);
-                    JSONArray resultStringTable = new JSONArray();
-                    resultStringTable.put(inArray.get(0));
-                    resultStringTable.put(inArray.get(1));
-                    resultStringTable.put(inArray.getInt(2) +10);
-                    resultStringTable.put(inArray.get(3));
-                    resultStringTable.put(inArray.get(4));
-                    result.put(i,resultStringTable);
-
-                }else {
-                    result.put(i,array.getJSONArray(i));
-                }
-            }
-            JSONObject toServer = new JSONObject();
-            toServer.put("result",result);
-            myRef.setValue(toServer.toString());
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
 }
--- app/src/main/java/com/pawa/aeroxo/ui/cheks/CheksViewModel.java
+++ app/src/main/java/com/pawa/aeroxo/ui/cheks/CheksViewModel.java
@@ -1,5 +1,23 @@
 package com.pawa.aeroxo.ui.cheks;
 
+import android.widget.ArrayAdapter;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.pawa.aeroxo.bd.Check;
+import com.pawa.aeroxo.bd.Track;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -7,13 +25,69 @@ import androidx.lifecycle.ViewModel;
 public class CheksViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<List<Check>>checksLD;
+    private List<Check>checks = new ArrayList<>();
+    private final int NAMECHECK = 0;
+    private final int DATE = 1;
+    private final int SUM = 2;
+    private final int OWNER = 3;
+    private final int STORAGECHECK = 4;
+    private final int STATUS = 5;
+    private final int COMMENT = 6;
+    private final int ISVIEWED = 7;
+    private String fullName;
+
 
     public CheksViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("Скоро тут что-то будет...");
+        //mText = new MutableLiveData<>();
+        //mText.setValue("Скоро тут что-то будет...");
+
+    }
+    public void startUpdate(String FullName){
+        fullName = FullName;
+        checksLD = new MutableLiveData<>();
+        updateChecks();
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+    private void updateChecks(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("Checks");
+        checks.clear();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                try {
+                    JSONObject reader = new JSONObject(value);
+                    JSONArray array = reader.getJSONArray("result");
+                    for(int i = 0;i<array.length();i++){
+                        JSONArray jsonArray = array.getJSONArray(i);
+                        if(jsonArray.getString(OWNER).equals(fullName)){
+                            Check check = new Check();
+                            check.nameCheck = jsonArray.getString(NAMECHECK);
+                            check.Date = jsonArray.getString(DATE);
+                            check.Sum  = Float.parseFloat(jsonArray.getString(SUM));
+                            check.Owner = jsonArray.getString(OWNER);
+                            check.storageCheck = jsonArray.getString(STORAGECHECK);
+                            check.Status = jsonArray.getString(STATUS);
+                            check.Comment = jsonArray.getString(COMMENT);
+                            check.isViewed = Boolean.parseBoolean(jsonArray.getString(ISVIEWED));
+                            checks.add(check);
+                        }
+                    }
+                    checksLD.setValue(checks);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public LiveData<List<Check>>getChecks(){return checksLD;}
 }
