64a1c4702b6b2168c3e71f9a484e4d2533d76946
Pawaus <pawa.usov2013@gmail.com>
2020-08-16 17:54:50 +0300

    check

---
+++ app/src/main/java/com/pawa/aeroxo/bd/Check.java
@@ -0,0 +1,12 @@
+package com.pawa.aeroxo.bd;
+
+public class Check {
+    public String nameCheck;
+    public String Date;
+    public Float Sum;
+    public String Owner;
+    public String storageCheck;
+    public String Status;
+    public String Comment;
+    public boolean isViewed;
+}
